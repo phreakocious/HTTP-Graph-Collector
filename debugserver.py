6ba25f8ce36307f6e21dd0c67192d305864f4f11
phreakocious <phreakocious@users.noreply.github.com>
2022-07-19 14:36:13 -0500

    update debugserver.py for python3

--- debugserver.py
+++ debugserver.py
@@ -1,19 +1,20 @@
-import SimpleHTTPServer
-import SocketServer
+#!/usr/bin/env python3
+
+import http.server
+import socketserver
 
 PORT = 65444
 
-class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class ServerHandler(http.server.SimpleHTTPRequestHandler):
 
     def do_POST(self):
-		self.send_response(200)
- 		content_len = int(self.headers.getheader('content-length', 0))
-		post = self.rfile.read(content_len)
-		print post
+        self.send_response(200)
+        content_len = int(self.headers.get('content-length', 0))
+        post = self.rfile.read(content_len)
+        print(post)
 
-Handler = ServerHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "server listening on port", PORT
-httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), ServerHandler) as httpd:
+    print("server listening on port", PORT)
+    httpd.serve_forever()
