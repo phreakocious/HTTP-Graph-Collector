5e6ee4745c110b5253b19c0209a572d43f990cb5
phreakocious <phreakocious@users.noreply.github.com>
2022-09-07 11:21:40 -0500

    update batch size for loader and convert the tabs to spaces... why were they tabs?

--- httpgraph-loader.py
+++ httpgraph-loader.py
@@ -14,20 +14,20 @@ import time
 from urllib import request
 from tqdm import tqdm
 
-batch_size = 100
+batch_size = 500
 file = "httpgraph-requests.json"
 url = "http://localhost:65444/add_record"
 
 with open(file) as f:
-	line_gen = zip(*[f]*batch_size)
-	for lines in tqdm(line_gen):
-		lines = { l.rstrip("\r") for l in lines }
-		data = ''.join(map(str, lines))
-		req = request.Request(url=url,
-		                      data=data.encode("utf-8"),
-		                      headers={"Content-Type":
-		                               "application/json; charset=utf-8"})
-		with request.urlopen(req) as f:
-			pass
+    line_gen = zip(*[f]*batch_size)
+    for lines in tqdm(line_gen):
+        lines = {line.rstrip("\r") for line in lines}
+        data = ''.join(map(str, lines)).encode("utf-8")
+        req = request.Request(url=url,
+                              data=data,
+                              headers={"Content-Type":
+                                       "application/json; charset=utf-8"})
+        with request.urlopen(req) as f:
+            pass
 
-		time.sleep(.02)
+        time.sleep(.1)
