69927ea44fdaffb39689c78f846cb48993509afd
phreakocious <phreakocious@users.noreply.github.com>
2024-04-22 16:20:30 -0500

    make httpgraph-loader smarter about batching

--- httpgraph-loader.py
+++ httpgraph-loader.py
@@ -11,6 +11,7 @@
 #
 
 import time
+import itertools
 from urllib import request
 from tqdm import tqdm
 
@@ -18,21 +19,16 @@ batch_size = 500
 file = "httpgraph-requests.json"
 url = "http://localhost:65444/add_record"
 
-with open(file, 'r', encoding="utf-8") as f:
+with open(file, "r", encoding="utf-8") as f:
     pbar = tqdm(total=len(f.readlines()), unit=" URLs")
     f.seek(0)
 
-    line_gen = zip(*[f]*batch_size)
-
-    for lines in line_gen:
-        lines = {line.rstrip("\r") for line in lines}
-        data = ''.join(map(str, lines)).encode("utf-8")
-        req = request.Request(url=url,
-                              data=data,
-                              headers={"Content-Type":
-                                       "application/json; charset=utf-8"})
+    for lines in itertools.zip_longest(*[f] * batch_size, fillvalue=None):
+        lines = tuple(line.rstrip("\r") for line in lines if line)
+        data = "".join(map(str, lines)).encode("utf-8")
+        req = request.Request(
+            url=url, data=data, headers={"Content-Type": "application/json; charset=utf-8"}
+        )
         with request.urlopen(req) as f:
-            pass
-
-        pbar.update(batch_size)
-        time.sleep(.1)
+            pbar.update(len(lines))
+            time.sleep(0.1)
